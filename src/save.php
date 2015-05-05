cdd14188d1ebc45c5497ad1d6bf1dab5e97964d8
Jan Henckens <jan@onedge.be>
2015-05-05 07:38:13 +0200

    Move save script to src

--- src/save.php
+++ src/save.php
@@ -1,12 +1,12 @@
 <?php
-require_once('../workflows.php');
+require_once('workflows.php');
 $wf = new Workflows();
 
 $data = explode( ";", $argv[1] );
 
 $key = $data['0'];
 $data = array( 'key' => $key );
-$save = $wf->write($data, '../user.ini');
+$save = $wf->write($data, 'user.ini');
 
 echo "saved!";
 
