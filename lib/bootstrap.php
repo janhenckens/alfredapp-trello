76f4d31b8c356d46b3bf39ec613f0f828e77ea8c
Jan Henckens <jan@onedge.be>
2015-05-09 12:47:50 +0200

    Add Trello PHP library

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -5,8 +5,10 @@
  * Date: 08/05/15
  * Time: 22:14
  */
+require_once ( './vendor/autoload.php' );
 
 require_once ( 'lib/base.php' );
 require_once ( 'lib/workflows.php' );
 require_once ( 'lib/app.php' );
+require_once ( 'lib/trello.php' );
 require_once ( 'lib/setup.php' );
